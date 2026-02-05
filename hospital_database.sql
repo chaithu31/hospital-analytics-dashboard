9290870d21911fe7edca820b9445bb8ca18ee45a
chaithu31 <142654122+chaithu31@users.noreply.github.com>
2026-02-05 12:40:11 +0530

    Add hospital database SQL schema and data

---
+++ hospital_database.sql
@@ -0,0 +1,32 @@
+{
+    "Servers": {
+        "1": {
+            "Name": "PostgreSQL 16",
+            "Group": "Servers",
+            "Host": "localhost",
+            "Port": 5432,
+            "MaintenanceDB": "postgres",
+            "Username": "postgres",
+            "Comment": "Auto-detected PostgreSQL 16 installation with the data directory at C:\\Program Files\\PostgreSQL\\16\\data",
+            "KerberosAuthentication": false,
+            "ConnectionParameters": {
+                "sslmode": "prefer",
+                "connect_timeout": 10
+            }
+        },
+        "2": {
+            "Name": "PostgreSQL 18",
+            "Group": "Servers",
+            "Host": "localhost",
+            "Port": 5433,
+            "MaintenanceDB": "postgres",
+            "Username": "postgres",
+            "Comment": "Auto-detected PostgreSQL 18 installation with the data directory at C:\\Program Files\\PostgreSQL\\18\\data",
+            "KerberosAuthentication": false,
+            "ConnectionParameters": {
+                "sslmode": "prefer",
+                "connect_timeout": 10
+            }
+        }
+    }
+}
